16938a4041027c373615d790b0fc816fd783a959
rania <raniaarbash@hotmail.com>
2026-02-26 20:46:43 -0500

    Week 7 Projects

---
+++ Week7_V1_XML/app/src/main/java/com/example/week7_v1_xml/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.week7_v1_xml;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName, etAge;
+    Button btnShow;
+    LinearLayout profileCard;
+    TextView tvName, tvAge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find views
+        etName = findViewById(R.id.etName);
+        etAge = findViewById(R.id.etAge);
+        btnShow = findViewById(R.id.btnShow);
+        profileCard = findViewById(R.id.profileCard);
+        tvName = findViewById(R.id.tvName);
+        tvAge = findViewById(R.id.tvAge);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = etName.getText().toString();
+                String age = etAge.getText().toString();
+
+                if (!name.isEmpty() && !age.isEmpty()) {
+
+                    tvName.setText("Name: " + name);
+                    tvAge.setText("Age: " + age);
+
+                    profileCard.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+}
---
+++ Week7_V2_JetpackCompose/app/src/main/java/com/example/week7_v2_jetpackcompose/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.week7_v2_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.week7_v2_jetpackcompose.ui.theme.Week7_V2_JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Week7_V2_JetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    studentProfile()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun studentProfile(){
+    var name by remember { mutableStateOf("") } // string state var
+    var age by remember { mutableStateOf("") }// string state var
+    var showProfile by remember { mutableStateOf(false ) } // bool state var
+   Column(
+       modifier = Modifier.fillMaxSize().padding(20.dp),
+       verticalArrangement = Arrangement.Center,
+       horizontalAlignment = Alignment.CenterHorizontally) {
+       Text("Student Profile")
+       Spacer(modifier = Modifier.height(20.dp))
+       OutlinedTextField( value = name,
+           onValueChange = {name = it },
+           label = {Text("Enter Your Name")})
+       Spacer(modifier = Modifier.height(20.dp))
+       OutlinedTextField( value = age,
+           onValueChange = {age = it },
+           label = {Text("Enter Your Age")})
+       Spacer(modifier = Modifier.height(20.dp))
+       Button(onClick = {
+           // make the profile card visible
+           showProfile = !showProfile
+       }) {
+           Text("Show Profile")
+       }
+       Spacer(modifier = Modifier.height(20.dp))
+       if (showProfile && name.isNotEmpty() && age.isNotEmpty()) {
+           Card(modifier = Modifier.fillMaxWidth()) {
+               Column(modifier = Modifier.padding(16.dp),
+                   verticalArrangement = Arrangement.Center,
+                   horizontalAlignment = Alignment.CenterHorizontally) {
+                   Text("Student Info")
+                   Text("Name: ${name}")
+                   Text("Age: ${age}")
+               }
+           }
+       }
+   }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Week7_V2_JetpackComposeTheme {
+        studentProfile()
+    }
+}
